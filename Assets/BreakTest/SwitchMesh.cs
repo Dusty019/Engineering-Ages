6bfdc2ff5f93a300bb450f9f3886a1e677cfc4e0
adamb70 <adam.boaler@hotmail.com>
2020-03-30 23:50:57 +0100

    initial commit

---
+++ Assets/BreakTest/SwitchMesh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMesh : MonoBehaviour
+{
+
+    public GameObject otherMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        otherMesh.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+        {
+            otherMesh.SetActive(true);
+            otherMesh.transform.SetParent(null);
+            Destroy(gameObject);
+        }
+    }
+}
